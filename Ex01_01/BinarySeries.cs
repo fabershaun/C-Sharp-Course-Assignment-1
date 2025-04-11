b4fd78af006908af9c044a2cafd800f5ffba85d0
fabershaun <fabershaun@gmail.com>
2025-04-11 15:01:09 +0300

    before changing to dictionary

--- Ex01_01/BinarySeries.cs
+++ Ex01_01/BinarySeries.cs
@@ -1,58 +1,126 @@
 ﻿using System;
+using System.Collections.Generic;                   //Check!
 
 namespace Ex01_01
 {
     internal class BinarySeries
     {
-        protected const int k_NumOfInputs = 4;
+
         protected const int k_BinaryNumberLength = 7;
-        public static void GetInputFromUser(ref int[] numbers)
+
+        struct NumberDetails
         {
-            String newLine = Environment.NewLine;
+            public int decimalFormat;
+            public int numOfOnesSeries;
+            public int numOfTransitions;
+            public int numOfOnes;
+        }
+
+        public static void GetInputFromUser(ref string[] io_numbers)
+        {
             Console.WriteLine("Please enter 4 numbers, 7 digits each, in a binary format");
 
-            for (int i = 0; i < k_NumOfInputs; i++)
+            for (int i = 0; i < io_numbers.Length; i++)
             {
-                // Check if the input is: 
-                // 1- a number
-                // 2- binary
-                // 3- length of 7 digits
-                String inputFromUser = Console.ReadLine();
+                string inputFromUser = Console.ReadLine();
 
-                while (int.TryParse(inputFromUser, out numbers[i]) == false)
+                while (CheckInputFromUser(ref inputFromUser, ref io_numbers, i) == false)
                 {
                     Console.WriteLine(string.Format(
 @"Invalid input.
 Please enter 4 numbers, 7 digits each, in a binary format"));
+
+                    inputFromUser = Console.ReadLine();
                 }
             }
         }
 
-        public static void ConvertBinaryToDecimal(ref int[] numbers)
+        public static bool CheckInputFromUser(ref string i_input, ref string[] io_numbers, int i_index)
         {
+            bool validInput = true;
+            int number;
+            if (int.TryParse(i_input, out number) == false)      // Check if it's a valid number
+            {
+                validInput = false;
+            }
+            else if (CheckIfNumberIsBinary(i_input) == false)    // Check if the number is binary
+            {
+                validInput = false;
+            }
+            else if (i_input.Length != k_BinaryNumberLength)    // Check if the length is 7
+            {
+                validInput = false;
+            }
 
-            for (int i = 0; i < k_NumOfInputs; i++)
+            return validInput;
+        }
+
+        public static bool CheckIfNumberIsBinary(string io_numberToCheck)
+        {
+            bool isBinary = true;
+            foreach (char currentCharInString in io_numberToCheck)
+            {
+                if (currentCharInString != '0' && currentCharInString != '1')
+                {
+                    isBinary = false;
+                    break;
+                }
+            }
+            return isBinary;
+        }
+
+        public static void ConvertBinaryToDecimal(ref int[] io_numbers)
+        {
+            for (int i = 0; i < io_numbers.Length; i++)
             {
                 int decimalNumber = 0;
 
                 for (int j = 0; j < k_BinaryNumberLength; j++)
                 {
-                    int digit = numbers[i] % 10;
+                    int digit = io_numbers[i] % 10;
                     digit = digit * (int)Math.Pow(2, j);
 
                     decimalNumber += digit;
-                    numbers[i] /= 10;
+                    io_numbers[i] /= 10;
                 }
 
-                numbers[i] = decimalNumber;
+                io_numbers[i] = decimalNumber;
             }
         }
 
-        public static void PrintNumbers(ref int[] numbers)
+        public static void PrintNumbersArray(ref int[] io_numbers)
         {
-            for (int i = 0; i < k_NumOfInputs; i++)
+            Console.WriteLine(Environment.NewLine + "The decimal values of the input numbers is: ");
+
+            foreach (int number in io_numbers)
             {
-                Console.WriteLine(numbers[i]);
+                Console.WriteLine(number);
+            } 
+        }
+        
+        public static void PrintAverageValueDecimal(ref int[] io_numbers)
+        {
+            float averageOfNumbersArray = CalculateAverageValues(ref io_numbers);
+            Console.WriteLine(Environment.NewLine + "The average of the input numbers is: " + averageOfNumbersArray);
+        }
+
+        public static float CalculateAverageValues(ref int[] io_numbers)
+        {
+            float sum = 0;
+
+            foreach (int number in io_numbers)
+            {
+                sum += number;
+            }
+            return sum / io_numbers.Length;
+
+        }
+
+        public static void AnalyzeNumbers(ref int[] i_numbers)
+        {
+            foreach (int number in i_numbers)
+            {
+
             }
         }
     }
--- Ex01_01/Program.cs
+++ Ex01_01/Program.cs
@@ -4,14 +4,20 @@ namespace Ex01_01
 {
     internal class Program
     {
+        protected const int k_NumOfInputs = 4;
         public static void Main()
         {
-            int[] numbers = new int[4];
+
+            string[] numbers = new string[k_NumOfInputs];
             BinarySeries.GetInputFromUser(ref numbers);
+
+            BinarySeries.AnalyzeNumbers(ref numbers);
+
             BinarySeries.ConvertBinaryToDecimal(ref numbers);
-            Array.Sort(numbers);                                //TODO: check if ok
-            Array.Reverse(numbers);                                //TODO: check if ok
-            BinarySeries.PrintNumbers(ref numbers); 
+            Array.Sort(numbers);                                
+            Array.Reverse(numbers);                                
+            BinarySeries.PrintNumbersArray(ref numbers);
+            BinarySeries.PrintAverageValueDecimal(ref numbers);
 
         }
     }
