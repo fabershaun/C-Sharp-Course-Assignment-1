8ee598bf9d07bb77b77b9a36223d1f6ed911bda0
fabershaun <fabershaun@gmail.com>
2025-04-11 09:24:16 +0300

    start

---
+++ Ex01_01/BinarySeries.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Ex01_01
+{
+    internal class BinarySeries
+    {
+        protected const int k_NumOfInputs = 4;
+        protected const int k_BinaryNumberLength = 7;
+        public static void GetInputFromUser(ref int[] numbers)
+        {
+            String newLine = Environment.NewLine;
+            Console.WriteLine("Please enter 4 numbers, 7 digits each, in a binary format");
+
+            for (int i = 0; i < k_NumOfInputs; i++)
+            {
+                // Check if the input is: 
+                // 1- a number
+                // 2- binary
+                // 3- length of 7 digits
+                String inputFromUser = Console.ReadLine();
+
+                while (int.TryParse(inputFromUser, out numbers[i]) == false)
+                {
+                    Console.WriteLine(string.Format(
+@"Invalid input.
+Please enter 4 numbers, 7 digits each, in a binary format"));
+                }
+            }
+        }
+
+        public static void ConvertBinaryToDecimal(ref int[] numbers)
+        {
+
+            for (int i = 0; i < k_NumOfInputs; i++)
+            {
+                int decimalNumber = 0;
+
+                for (int j = 0; j < k_BinaryNumberLength; j++)
+                {
+                    int digit = numbers[i] % 10;
+                    digit = digit * (int)Math.Pow(2, j);
+
+                    decimalNumber += digit;
+                    numbers[i] /= 10;
+                }
+
+                numbers[i] = decimalNumber;
+            }
+        }
+
+        public static void PrintNumbers(ref int[] numbers)
+        {
+            for (int i = 0; i < k_NumOfInputs; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+        }
+    }
+}
---
+++ Ex01_01/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Ex01_01
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            int[] numbers = new int[4];
+            BinarySeries.GetInputFromUser(ref numbers);
+            BinarySeries.ConvertBinaryToDecimal(ref numbers);
+            Array.Sort(numbers);                                //TODO: check if ok
+            Array.Reverse(numbers);                                //TODO: check if ok
+            BinarySeries.PrintNumbers(ref numbers); 
+
+        }
+    }
+}
