d56b7ec7fd089a5e46486a6b591a2a1b5bec72fa
fabershaun <fabershaun@gmail.com>
2025-04-13 22:12:15 +0300

    finish 2

---
+++ Ex01_02/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex01_02
+{
+    internal class Program
+    {
+        private const int k_TreeHeight = 7;
+        public static void Main()
+        {
+            Tree.PrintTree(k_TreeHeight);
+        }
+    }
+}
---
+++ Ex01_02/Tree.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Ex01_02
+{
+    internal class Tree
+    {
+        public static void PrintTree(int i_Height)
+        {
+            int numberOfCharsInLine = 1;
+            int numberOfSpaces = i_Height * 2 - 4;
+            int numberToStart = 1;
+            char rowLetter = 'A';
+
+            printTreeHelper(i_Height, numberOfCharsInLine, numberToStart, rowLetter, numberOfSpaces, numberOfSpaces);
+
+        }
+        private static void printTreeHelper(int  i_Height, int i_NumberOfCharsInLine, int i_NumberToStart,
+            char i_RowLetter, int i_NumberOfSpaces, int i_NumberOfSpacesOriginal)
+        {
+            if (i_Height == 2)
+            {
+                for (int i = 0; i < 2; i++)
+                {
+                    Console.Write(i_RowLetter++);
+                    Console.Write(new string(' ', i_NumberOfSpacesOriginal));
+                    Console.WriteLine("|{0}|", i_NumberToStart);
+                }
+
+                return;
+            }
+
+            Console.Write(i_RowLetter);
+            Console.Write(new string(' ', i_NumberOfSpaces));       // Printing spaces
+
+            for (int i = 0; i < i_NumberOfCharsInLine; i++)
+            {
+                if (i_NumberToStart == 10)
+                {
+                    i_NumberToStart = 1;
+                }
+                Console.Write(" {0}", i_NumberToStart++);
+            }
+
+            Console.WriteLine();
+
+            printTreeHelper(i_Height - 1, i_NumberOfCharsInLine + 2,
+                i_NumberToStart, (char)(i_RowLetter + 1), i_NumberOfSpaces - 2, i_NumberOfSpacesOriginal);
+        }
+    }
+}
